e8b0df0e3156098c5a0bfaf970b4574ff2066bb7
Chris Stella <chris.stella.84@gmail.com>
2023-06-23 23:00:22 -0700

    added remote backend

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "hashicorp-testorg-01"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "customer-iac-demo"
+    }
+  }
+}
+
 # Authenticate with HCP Vault
 provider "vault" {
   namespace = var.vault_namespace
